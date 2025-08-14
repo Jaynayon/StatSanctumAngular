1eeaa9b3d48aa579a9773ed0b739ded46e7a5efc
Jaynayon <jay.nayonjr@cit.edu>
2025-08-15 03:06:24 +0800

    Add Form control and icon

--- src/app/sign-in-modal/sign-in-modal.component.ts
+++ src/app/sign-in-modal/sign-in-modal.component.ts
@@ -1,14 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in-modal',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-in-modal.component.html',
   styleUrl: './sign-in-modal.component.css'
 })
 export class SignInModalComponent {
   @Output() close = new EventEmitter<void>();
+
+  emailControl = new FormControl('');
+  passwordControl = new FormControl('');
+
+  onSubmit() {
+    console.log(this.emailControl.value);
+    console.log(this.passwordControl.value);
+  }
 
   onClose() {
     this.close.emit();
