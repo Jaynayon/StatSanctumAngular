a0de3a9959dbc52aab111eba239795bc490c610d
Jaynayon <jay.nayonjr@cit.edu>
2025-08-18 16:45:47 +0800

    Partial Integration for front and backend

--- src/app/sign-in-modal/sign-in-modal.component.ts
+++ src/app/sign-in-modal/sign-in-modal.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-sign-in-modal',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './sign-in-modal.component.html',
   styleUrl: './sign-in-modal.component.css'
 })
@@ -14,7 +16,55 @@ export class SignInModalComponent {
   emailControl = new FormControl('');
   passwordControl = new FormControl('');
 
+  forgotPassword = false;
+  signUp = false;
+
+  constructor(private authService: AuthService) { }
+
+  get title() {
+    return this.signUp ? "Sign Up" : "Sign In";
+  }
+
+  get titleLowercase() {
+    return this.signUp ? "Sign up" : "Sign in";
+  }
+
+  get entryLink() {
+    return this.signUp ? "Sign in" : "Sign up";
+  }
+
+  get entryLinkAdverb() {
+    return this.signUp ? "Already" : "Don't";
+  }
+
+  onRegisterPress(event: Event) {
+    this.signUp = !this.signUp;
+    console.log("register clicked", this.signUp);
+  }
+
+  onForgotPress(event: Event) {
+    this.forgotPassword = !this.forgotPassword;
+    console.log("Forgot password clicked", this.forgotPassword);
+  }
+
   onSubmit() {
+    if (this.signUp) {
+      console.log("Query for signing up");
+    } else {
+      console.log("Query for signing in");
+      this.authService.login(this.emailControl.value!, this.passwordControl.value!).subscribe({
+        next: (response) => {
+          console.log('Login successful', response);
+          // this.isLoading = false;
+          // Handle successful login (redirect, store token, etc.)
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+          // this.isLoading = false;
+          // this.errorMessage = error.error?.message || 'Login failed';
+        }
+      })
+    }
     console.log(this.emailControl.value);
     console.log(this.passwordControl.value);
   }
